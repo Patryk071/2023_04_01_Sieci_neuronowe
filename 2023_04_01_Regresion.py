65fb5307c714079acb7d94c1eb716ca8d7ee0020
Patryk_G <pw28358@student.wsb.wroclaw.pl>
2023-04-01 13:33:06 +0200

    2023_04_01_Sieci_neuronowe_005

--- 2023_04_01_Regresion.py
+++ 2023_04_01_Regresion.py
@@ -7,13 +7,35 @@ from tensorflow.random import set_seed
 
 set_seed(0)
 
+# Komponenty sieci
 model = Sequential()
-
 model.add(Dense(4, input_shape=[1], activation="linear"))
 model.add(Dense(2, activation="linear"))
 model.add(Dense(1))
 
+# Kompilacja modelu
 model.compile(optimizer="rmsprop", loss="mse", metrics=["mae"])
-
 df = pd.read_csv("https://vesemir.wiedzmin.net/extras/f-c.csv")
+
 print(df.head())
+
+plt.scatter(df.F, df.C)
+plt.show()
+
+# Uczenie sieci
+result = model.fit(df.F, df.C, epochs=1500, verbose=0)
+
+print(result.history.keys())
+
+df1 = pd.DataFrame(result.history)
+print(df1.head(500))
+
+df1.plot()
+plt.show()
+
+y_pred = model.predict(df.F)
+
+plt.scatter(df.F, df.C)
+
+plt.plot(df.F, y_pred, c='r')
+plt.show()
